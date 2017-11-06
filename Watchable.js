62cee61ef1fac48663627895ab826f24c22435cc
Don <don@thegriffins.org>
2017-11-05 21:11:03 -0600

    Initial commit

---
+++ Watchable.js
@@ -0,0 +1,206 @@
+'use strict';
+
+const actualFnSym = Symbol('actualListener');
+const firingSym = Symbol('eventFiring');
+const watchersSym = Symbol('eventWatchers');
+const STOP = Object.freeze({});
+
+function Empty (props) {
+    if (props) {
+        Object.assign(this, props);
+    }
+}
+
+Empty.prototype = Object.create(null);
+
+//----------------------------------------
+
+class Destroyer {
+    constructor (instance) {
+        this.watchable = instance;
+        this.entries = [];
+    }
+
+    destroy () {
+        let instance = this.watchable;
+        let watcherMap = instance[watchersSym];
+        let entry, index, name, watchers;
+
+        if (watcherMap) {
+            for (entry of this.entries) {
+                watchers = watcherMap[name = entry[2]];
+
+                if (watchers && (index = watchers.indexOf(entry)) > -1) {
+                    if (watchers[firingSym]) {
+                        watcherMap[name] = watchers = watchers.slice();
+                        watchers[firingSym] = 0;
+                    }
+
+                    watchers.splice(index, 1);
+                }
+            }
+        }
+    }
+}
+
+function call (fn, scope, args) {
+    return fn.charAt ? scope[fn](...args) : fn.call(scope, ...args);
+}
+
+function on (watcherMap, name, fn, scope, destroyer) {
+    let watchers = watcherMap[name];
+    let actualFn, entry;
+
+    if (watchers) {
+        for (entry of watchers) {
+            actualFn = entry[0];
+            actualFn = actualFn[actualFnSym] || actualFn;
+
+            if (actualFn === fn && (scope ? entry[1] === scope : !entry[1])) {
+                return;
+            }
+        }
+
+        if (watchers[firingSym]) {
+            watcherMap[name] = watchers = watchers.slice();
+            watchers[firingSym] = 0;
+        }
+    }
+    else {
+        watcherMap[name] = watchers = [];
+        watchers[firingSym] = 0;
+    }
+
+    watchers.push(entry = [fn, scope]);
+
+    if (destroyer) {
+        destroyer.entries.push(entry);
+        entry.push(name);
+    }
+}
+
+function un (watcherMap, name, fn, scope) {
+    let entry, i, watchers = watcherMap[name];
+
+    if (watchers) {
+        if (watchers[firingSym]) {
+            watcherMap[name] = watchers = watchers.slice();
+            watchers[firingSym] = 0;
+        }
+
+        for (i = watchers.length; i-- > 0; ) {
+            entry = watchers[i];
+
+            if (entry[0] === fn && (scope ? entry[1] === scope : !entry[1])) {
+                watchers.splice(i, 1);
+            }
+        }
+    }
+}
+
+function update (instance, updater, name, fn, scope) {
+    let watcherMap = instance[watchersSym];
+    let add = updater === on;
+    let destroyer = null;
+
+    if (!watcherMap) {
+        if (!add) {
+            return destroyer;
+        }
+
+        instance[watchersSym] = watcherMap = new Empty();
+    }
+
+    if (typeof name === 'string') {
+        updater(instance, watcherMap, name, fn, scope);
+    }
+    else {
+        // "name" is a manifest object of watchers
+
+        destroyer = add && new Destroyer(instance);
+        scope = name.scope;
+
+        for (let s of Object.keys(name)) {
+            if (!Watchable.options[s]) {
+                updater(instance, watcherMap, s, name[s], scope, destroyer);
+            }
+        }
+    }
+
+    return destroyer;
+}
+
+//----------------------------------------
+
+const descriptors = {};
+
+class Watchable {
+    static applyTo (target) {
+        Object.defineProperties(target, descriptors);
+        target[watchersSym] = null;
+    }
+
+    emit (event, ...args) {
+        return this.fire(event, ...args);
+    }
+
+    fire (event, ...args) {
+        let watcherMap = this[watchersSym];
+        let watchers = watcherMap && watcherMap[event];
+        let ret;
+
+        if (watchers) {
+            ++watchers[firingSym];
+
+            for (let entry of watchers) {
+                if (call(entry[0], entry[1], args) === STOP) {
+                    ret = STOP;
+                    break;
+                }
+            }
+
+            --watchers[firingSym];
+        }
+
+        return ret;
+    }
+
+    on (name, fn, scope) {
+        return update(this, on, name, fn, scope);
+    }
+
+    once (name, fn, scope) {
+        const me = this;
+
+        function onceFn (...args) {
+            update(me, un, name, onceFn, scope);
+            call(fn, scope, args);
+        }
+
+        onceFn[actualFnSym] = fn;
+
+        return update(me, on, name, onceFn, scope);
+    }
+
+    off (name, fn, scope) {
+        return update(this, un, name, fn, scope);
+    }
+
+    un (name, fn, scope) {
+        return update(this, un, name, fn, scope);
+    }
+}
+
+let proto = Watchable.prototype;
+
+for (let name of Object.getOwnPropertyNames(proto)) {
+    descriptors[name] = Object.getOwnPropertyDescriptor(proto, name);
+}
+
+Watchable.options = new Empty({
+    scope: true
+});
+
+Watchable.STOP = STOP;
+
+module.exports = Watchable;
